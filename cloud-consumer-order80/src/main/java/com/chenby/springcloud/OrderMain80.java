86e106edb4c39c9cafc77ef6b9b5dcd003f78b05
chenby228 <chenby228@163.com>
2020-04-03 11:53:19 +0800

    springcloud-study

---
+++ cloud-consumer-order80/src/main/java/com/chenby/springcloud/OrderMain80.java
@@ -0,0 +1,18 @@
+package com.chenby.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-18 17:20
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class OrderMain80
+{
+    public static void main(String[] args) {
+            SpringApplication.run(OrderMain80.class, args);
+    }
+}
